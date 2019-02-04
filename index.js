48b5149dda2adc5563f4ba792295b3781a5d2211
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-04 11:53:25 -0500

    Save

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Using a regular function instead of an arrow function allow us to bind (this) and retain context
-export const strToTag = function(str: string, tag: any) {
+export const strToTag = function(str, tag) {
   return new Function("tag", "return tag`" + str + "`").bind(this)(tag);
 };
